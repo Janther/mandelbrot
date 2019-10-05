a8c3870e80fda580a82d7b3e8861aad1c06c5432
Klaus Hott Vidal <klahott@gmail.com>
2019-10-05 17:15:21 +1300

    first plots

---
+++ build/precache-manifest.a12e0082f88049492d8ea9f25d946417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d116439c82705b494f34562a3b5e822b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6507da67704118dd1cfb",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6a89bef6fa8733cd33d8",
+    "url": "/static/js/2.b8f8b7af.chunk.js"
+  },
+  {
+    "revision": "6507da67704118dd1cfb",
+    "url": "/static/js/main.72cef5a4.chunk.js"
+  },
+  {
+    "revision": "296e8bca358085a9e042",
+    "url": "/static/js/runtime-main.53d3505f.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
 
 function App() {
+  const canvasHeight = 800;
+  const canvasWidth = 1000;
+  const scale = Math.min(canvasHeight, canvasWidth) / 2.5;
+
+  let [base, setBase] = useState(10);
+  let [multiplier, setMultiplier] = useState(2);
+  let [connections, setConnections] = useState([]);
+
+  const getAngle = (x, base) => (x / base) * 2 * Math.PI;
+
+  const getXY = useCallback(
+    angle => ({
+      x: Math.cos(angle) * scale + canvasWidth / 2,
+      y: Math.sin(angle) * scale + canvasHeight / 2
+    }),
+    [scale, canvasWidth, canvasHeight]
+  );
+
+  const pointFactory = useCallback(
+    (x, base) => {
+      let angle = getAngle(x, base);
+      return getXY(angle);
+    },
+    [getXY]
+  );
+
+  useEffect(() => {
+    let newConnections = [];
+    for (let i = 0; i < base; i++) {
+      newConnections.push({
+        p1: pointFactory(i + 1, base),
+        p2: pointFactory((i + 1) * multiplier, base)
+      });
+    }
+    setConnections(newConnections);
+  }, [multiplier, base, scale, pointFactory]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg
+        width={canvasWidth}
+        height={canvasHeight}
+        style={{ border: "1px solid black;" }}
+      >
+        {connections.map((line, i) => (
+          <line
+            x2={line.p2.x}
+            y2={line.p2.y}
+            x1={line.p1.x}
+            y1={line.p1.y}
+            stroke-width="1"
+            stroke="black"
+            key={i}
+          ></line>
+        ))}
+      </svg>
+      <input
+        type="number"
+        value={base}
+        onChange={e => setBase(parseInt(e.target.value))}
+      />
+      <input
+        type="number"
+        value={multiplier}
+        step=".1"
+        onChange={e => setMultiplier(parseFloat(e.target.value))}
+      />
     </div>
   );
 }
